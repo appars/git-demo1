366228057bc2c7a5aa4f7cc6482a875aad0da701
Apparsamy Perumal <apparsamy.p@cmrit.ac.in>
2026-09-22 10:07:57 +0530

    adding StudentProfile.java

---
+++ StudentProfile.java
@@ -0,0 +1,15 @@
+public class StudentProfile {
+
+    public static void main(String[] args) {
+
+        String name = "Rahul";
+        String branch = "CSE";
+        int semester = 5;
+
+        System.out.println("Student Profile");
+        System.out.println("----------------");
+        System.out.println("Name      : " + name);
+        System.out.println("Branch    : " + branch);
+        System.out.println("Semester  : " + semester);
+    }
+}
